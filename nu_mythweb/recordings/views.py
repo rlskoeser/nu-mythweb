fc8ca752ecb22a7085541a528dce25ffda62ca80
rlskoeser <rlskoeser@gmail.com>
2026-01-05 18:07:16 -0500

    Preliminary dashboard

--- nu_mythweb/recordings/views.py
+++ nu_mythweb/recordings/views.py
@@ -44,29 +44,85 @@ def get_status_class(code):
         return "status-default"
 
 
-def upcoming_list(request):
+def get_upcoming_recordings(limit=None):
     url = f"http://{MYTHTV_BACKEND_IP}:{MYTHTV_PORT}/Dvr/GetUpcomingList"
+    params = {}
+    if limit is not None:
+        params["Count"] = limit
+
+    response = requests.get(
+        url, headers={"Accept": "application/json"}, params=params, timeout=5
+    )
+    response.raise_for_status()
+    data = response.json()
+    programs = data.get("ProgramList", {}).get("Programs", [])
+
+    for prog in programs:
+        # parse starttime and end time as datetime
+        prog["dt_start"] = datetime.fromisoformat(prog["StartTime"])
+        prog["dt_end"] = datetime.fromisoformat(prog["EndTime"])
+        # get recording status
+        recording_data = prog.get("Recording", {})
+        prog["status_display"] = split_camel_case(
+            recording_data.get("StatusName", "Unknown")
+        )
+        # convert numeric status to css class for display
+        prog["status_code_class"] = get_status_class(
+            int(recording_data.get("Status", 99))
+        )
+        # get category code
+        prog["category_code"] = category_slug(prog["Category"])
+
+    return programs
+
+
+def get_recent_recordings(limit=10):
+    url = f"http://{MYTHTV_BACKEND_IP}:{MYTHTV_PORT}/Dvr/GetRecordedList"
+    params = {"descending": True}
+    if limit is not None:
+        params["Count"] = limit
+
+    response = requests.get(
+        url, headers={"Accept": "application/json"}, params=params, timeout=5
+    )
+    response.raise_for_status()
+    data = response.json()
+    programs = data.get("ProgramList", {}).get("Programs", [])
+    for prog in programs:
+        # parse starttime and end time as datetime
+        prog["dt_start"] = datetime.fromisoformat(prog["StartTime"])
+        prog["dt_end"] = datetime.fromisoformat(prog["EndTime"])
+        # get recording status
+        recording_data = prog.get("Recording", {})
+        prog["status_display"] = split_camel_case(
+            recording_data.get("StatusName", "Unknown")
+        )
+        # convert numeric status to css class for display
+        prog["status_code_class"] = get_status_class(
+            int(recording_data.get("Status", 99))
+        )
+        # get category code
+        prog["category_code"] = category_slug(prog["Category"])
+
+    return programs
+
+
+def dashboard(request):
+    context = {"upcoming": [], "error": None}
+    try:
+        context["upcoming"] = get_upcoming_recordings(limit=3)
+        context["recorded"] = get_recent_recordings(limit=3)
+    except Exception as e:
+        context["error"] = f"Could not connect to MythTV: {e}"
+
+    return render(request, "recordings/dashboard.html", context)
+
+
+def upcoming_list(request):
     context = {"programs": [], "error": None}
 
     try:
-        response = requests.get(url, headers={"Accept": "application/json"}, timeout=5)
-        response.raise_for_status()
-        data = response.json()
-        context["programs"] = data.get("ProgramList", {}).get("Programs", [])
-        # parse starttime and end time as datetime
-        for prog in context["programs"]:
-            prog["dt_start"] = datetime.fromisoformat(prog["StartTime"])
-            prog["dt_end"] = datetime.fromisoformat(prog["EndTime"])
-            # get recording status
-            recording_data = prog.get("Recording", {})
-            prog["status_display"] = split_camel_case(
-                recording_data.get("StatusName", "Unknown")
-            )
-            # convert numeric status to css class for display
-            prog["status_code_class"] = get_status_class(
-                int(recording_data.get("Status", 99))
-            )
-            prog["category_code"] = category_slug(prog["Category"])
+        context["programs"] = get_upcoming_recordings()
     except Exception as e:
         context["error"] = f"Could not connect to MythTV: {e}"
 
--- nu_mythweb/urls.py
+++ nu_mythweb/urls.py
@@ -5,8 +5,9 @@ URL configuration for nu_mythweb project.
 # from django.contrib import admin
 from django.urls import path
 
-from nu_mythweb.recordings.views import upcoming_list
+from nu_mythweb.recordings.views import dashboard, upcoming_list
 
 urlpatterns = [
-    path("", upcoming_list, name="upcoming"),
+    path("", dashboard, name="home"),
+    path("upcoming/", upcoming_list, name="upcoming"),
 ]
