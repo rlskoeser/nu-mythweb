d8ceec3b859dfa8fcd65570ce7d0e4390a59e15f
rlskoeser <rlskoeser@gmail.com>
2026-01-13 22:24:33 -0500

    Remove debug print statements

--- nu_mythweb/settings.py
+++ nu_mythweb/settings.py
@@ -29,9 +29,6 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-print(os.getenv("DJANGO_DEBUG", "False"))
-print(os.getenv("DJANGO_DEBUG", "False") == "True")
-
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
 
 
